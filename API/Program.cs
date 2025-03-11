d334b022c4973abe02b029d49168f9b6e4b8fce4
Jakub <58849519+K-u-r-c@users.noreply.github.com>
2025-03-11 14:17:06 +0100

    add initial client setup with React, TypeScript, and Vite configuration

--- API/Program.cs
+++ API/Program.cs
@@ -8,8 +8,11 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
 app.MapControllers();
 
