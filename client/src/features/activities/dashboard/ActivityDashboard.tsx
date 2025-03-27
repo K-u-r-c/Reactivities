7a66751e560b03813e205c96d01785a59e345a71
Jakub <58849519+K-u-r-c@users.noreply.github.com>
2025-03-27 10:49:01 +0100

    add activity details page and filters; implement ActivityDetailsPage and ActivityFilters components, update routing and styles

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,5 +1,6 @@
 import { Grid2 } from "@mui/material";
 import ActivityList from "./ActivityList";
+import ActivityFilters from "./ActivityFilters";
 
 export default function ActivityDashboard() {
   return (
@@ -7,7 +8,9 @@ export default function ActivityDashboard() {
       <Grid2 size={7}>
         <ActivityList />
       </Grid2>
-      <Grid2 size={5}>Activity filters go here</Grid2>
+      <Grid2 size={5}>
+        <ActivityFilters />
+      </Grid2>
     </Grid2>
   );
 }
