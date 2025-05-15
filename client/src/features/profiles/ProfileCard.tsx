8e043a731b81b50d664bef68c4f1260439cf3430
Jakub <58849519+K-u-r-c@users.noreply.github.com>
2025-05-16 00:16:24 +0200

    feat: add DeleteButton component and integrate photo deletion functionality in ProfilePhotos component

--- client/src/features/profiles/ProfileCard.tsx
+++ client/src/features/profiles/ProfileCard.tsx
@@ -33,11 +33,23 @@ export default function ProfileCard({ profile }: Props) {
           component="img"
           src={profile?.imageUrl || "/images/user.png"}
           alt={profile.displayName + "image"}
-          sx={{ width: 200, zIndex: 50 }}
+          sx={{ width: "100%", zIndex: 50 }}
         />
         <CardContent>
-          <Box display="flex" alignItems="center" gap={1}>
+          <Box display="flex" flexDirection={"column"} gap={1}>
             <Typography variant="h5">{profile.displayName}</Typography>
+            {profile.bio && (
+              <Typography
+                variant="body2"
+                sx={{
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                  whiteSpace: "nowrap",
+                }}
+              >
+                {profile.bio}
+              </Typography>
+            )}
             {following && (
               <Chip
                 size="small"
--- client/src/features/profiles/ProfilePhotos.tsx
+++ client/src/features/profiles/ProfilePhotos.tsx
@@ -3,6 +3,7 @@ import { useProfile } from "../../lib/hooks/useProfile";
 import {
   Box,
   Button,
+  Divider,
   ImageList,
   ImageListItem,
   Typography,
@@ -10,6 +11,7 @@ import {
 import { useState } from "react";
 import PhotoUploadWidget from "../../app/shared/components/PhotoUploadWidget";
 import StarButton from "../../app/shared/components/StarButton";
+import DeleteButton from "../../app/shared/components/DeleteButton";
 
 export default function ProfilePhotos() {
   const { id } = useParams();
@@ -20,6 +22,7 @@ export default function ProfilePhotos() {
     uploadPhoto,
     profile,
     setMainPhoto,
+    deletePhoto,
   } = useProfile(id);
   const [editMode, setEditMode] = useState(false);
 
@@ -36,44 +39,62 @@ export default function ProfilePhotos() {
 
   return (
     <Box>
-      {isCurrentUser && (
-        <Box>
+      <Box display={"flex"} justifyContent={"space-between"}>
+        <Typography variant="h5">Photos</Typography>
+        {isCurrentUser && (
           <Button onClick={() => setEditMode(!editMode)}>
             {editMode ? "Cancel" : "Add photo"}
           </Button>
-        </Box>
-      )}
+        )}
+      </Box>
+      <Divider sx={{ my: 2 }} />
       {editMode ? (
         <PhotoUploadWidget
           uploadPhoto={handlePhotoUpload}
           loading={uploadPhoto.isPending}
         />
       ) : (
-        <ImageList sx={{ height: 450 }} cols={6} rowHeight={164}>
-          {photos.map((item) => (
-            <ImageListItem key={item.id}>
-              <img
-                srcSet={`${item.url.replace(
-                  "/upload/",
-                  "/upload/w_250,h_250,c_fill,f_auto,dpr_2,g_face/"
-                )}`}
-                src={`${item.url.replace(
-                  "/upload/",
-                  "/upload/w_250,h_250,c_fill,f_auto,g_face/"
-                )}`}
-                loading="lazy"
-              />
-              {isCurrentUser && (
-                <Box
-                  sx={{ position: "absolute", top: 0, left: 0 }}
-                  onClick={() => setMainPhoto.mutate(item)}
-                >
-                  <StarButton selected={item.url === profile?.imageUrl} />
-                </Box>
-              )}
-            </ImageListItem>
-          ))}
-        </ImageList>
+        <>
+          {photos.length === 0 ? (
+            <Typography>No photos added yet</Typography>
+          ) : (
+            <ImageList sx={{ height: 450 }} cols={6} rowHeight={164}>
+              {photos.map((item) => (
+                <ImageListItem key={item.id}>
+                  <img
+                    srcSet={`${item.url.replace(
+                      "/upload/",
+                      "/upload/w_250,h_250,c_fill,f_auto,dpr_2,g_face/"
+                    )}`}
+                    src={`${item.url.replace(
+                      "/upload/",
+                      "/upload/w_250,h_250,c_fill,f_auto,g_face/"
+                    )}`}
+                    loading="lazy"
+                  />
+                  {isCurrentUser && (
+                    <div>
+                      <Box
+                        sx={{ position: "absolute", top: 0, left: 0 }}
+                        onClick={() => setMainPhoto.mutate(item)}
+                      >
+                        <StarButton selected={item.url === profile?.imageUrl} />
+                      </Box>
+                      {profile?.imageUrl !== item.url && (
+                        <Box
+                          sx={{ position: "absolute", top: 0, right: 0 }}
+                          onClick={() => deletePhoto.mutate(item.id)}
+                        >
+                          <DeleteButton />
+                        </Box>
+                      )}
+                    </div>
+                  )}
+                </ImageListItem>
+              ))}
+            </ImageList>
+          )}
+        </>
       )}
     </Box>
   );
