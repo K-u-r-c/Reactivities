0c7eff6ac3451ca62628d392686aa8b657be842a
Jakub <58849519+K-u-r-c@users.noreply.github.com>
2025-05-24 13:08:04 +0200

    fix: change follow-list endpoint from POST to GET for proper HTTP method usage

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -56,7 +56,7 @@ public class ProfilesController : BaseApiController
         return HandleResult(await Mediator.Send(new FollowToggle.Command { TargetUserID = userId }));
     }
 
-    [HttpPost("{userId}/follow-list")]
+    [HttpGet("{userId}/follow-list")]
     public async Task<ActionResult> GetFollowings(string userId, string predicate)
     {
         return HandleResult(await Mediator.Send(
